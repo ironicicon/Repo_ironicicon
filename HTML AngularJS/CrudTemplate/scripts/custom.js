93cd75c3b9051a9dc8b191225dcea8700567d3d0
Ram <kennethcoder374@gmail.com>
2017-09-15 15:43:28 +0530

    CrudTemplate
    CrudTemplate

---
+++ HTML AngularJS/CrudTemplate/scripts/custom.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function(){	
+	if(window.location.pathname.indexOf('action.html')>-1){
+		//current page is action.html
+		var queryString=location.search;
+		switch (queryString.split('=')[1].toUpperCase()){
+			case 'EDIT':
+				$('#heading').html('EDIT Action');
+				var divContent=getContent('edit.html');
+				break;
+			case 'ADD':
+				$('#heading').html('ADD Action');
+				var divContent=getContent('add.html');
+				break;
+			case 'DELETE':
+				$('#heading').html('DELETE Action');
+				var divContent=getContent('delete.html');
+				break;
+			default:
+				window.location.href='a.html';
+				break;
+		}
+	}
+});
+
+function getContent(htmlValue){
+	$.ajax({
+		url:htmlValue,
+		method:'get',
+		complete:function(res){
+			$('#innerContent').html(res);
+		}
+	});
+}
---
+++ HTML AngularJS/CrudTemplate/scripts/game.js
@@ -0,0 +1,64 @@
+﻿var isGameStarted=false;
+var text="qwerty";
+var replacedText="";
+var chancesLeft=2;
+$(document).ready(function(){	
+	
+	$('.start').click(function(){
+		$('#iniText').fadeOut(1000,function(){
+			$('.start').fadeOut(500);
+			$('#game').fadeIn(500);
+		});
+	});
+	setInterval(function(){!isGameStarted?setTimeout(beginGame(),1000):clearInterval();},2000);
+	$('.hint').click(function(e){
+		var progressBarWidth=Math.round($('#progBar').width()/$('.progress').width()*100);
+		//hint must come from service
+		$('#'+e.currentTarget.id).html(e.currentTarget.id);
+		$('#progBar').width((progressBarWidth+33.3)+'%');
+	});
+	
+	$.ajax({
+		
+	});
+	$(window).keypress(function(e){
+		if(isGameStarted){
+			var ev=e.keyCode;
+			if(chancesLeft>0){
+				if(text.indexOf(String.fromCharCode(ev))>-1){
+					var ch=String.fromCharCode(ev);
+					replacedText=replacedText.replace(replacedText.charAt(text.indexOf(ch)),String.fromCharCode(ev));
+					$('#text').html('<h3 align="center">'+replacedText+'</h3>');
+				}
+				else{
+					$('#wrongcount').html('Chances Left:<span class="badge">'+chancesLeft+'</span>');
+					chancesLeft-=1;
+				}
+			}
+			else{
+				$('#game').fadeOut(100,function(){
+					$('#correctText').html(text);
+					$('#gameOver').fadeIn(50);
+					$('#tryAgain').fadeIn(50);
+				});
+			}
+		}
+	});
+	
+	$('#tryAgain').click(function(){
+		window.location.reload();
+	});
+});
+
+function beginGame(){
+	if($('#game').css('display')=='block'){
+		for(var i=0;i<text.length;i++){
+			replacedText+=text.charAt(i).replace(text.charAt(i),'_');
+		}
+		$('#wordcount').html("character count:"+text.length);
+		$('#text').html('<h3 align="center">'+replacedText+'</h3>');
+		isGameStarted=true;
+	}
+}
+
+
